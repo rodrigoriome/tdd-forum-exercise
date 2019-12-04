b40a0fbb54c8da4ac430b2c1d0b7572d761dd7da
Rodrigo Mesquita <rodrigoriome@gmail.com>
2019-12-03 23:01:14 -0300

    finish ep25

---
+++ app/RecordsActivity.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+trait RecordsActivity
+{
+    public static function bootRecordsActivity()
+    {
+        if (auth()->guest()) {
+            return;
+        }
+
+        foreach (static::getRecordableEvents() as $event) {
+            static::$event(function ($model) use ($event) {
+                $model->recordActivity($event);
+            });
+        }
+    }
+
+    protected static function getRecordableEvents()
+    {
+        return ['created'];
+    }
+
+    protected function recordActivity($event)
+    {
+        $this->activity()->create([
+            'type' => $this->getActivityTypeName($event),
+            'user_id' => auth()->id(),
+        ]);
+    }
+
+    protected function getActivityTypeName($event)
+    {
+        $type = strtolower((new \ReflectionClass($this))->getShortName());
+
+        return "{$event}_{$type}";
+    }
+
+    public function activity()
+    {
+        return $this->morphMany(Activity::class, 'subject');
+    }
+}
---
+++ tests/Unit/ActivityTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Activity;
+use App\Reply;
+use App\Thread;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ActivityTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function test_it_records_activity_when_a_thread_is_created()
+    {
+        $this->signIn();
+
+        $thread = create(Thread::class);
+
+        $this->assertDatabaseHas('activities', [
+            'type' => 'created_thread',
+            'user_id' => auth()->id(),
+            'subject_id' => $thread->id,
+            'subject_type' => Thread::class,
+        ]);
+
+        $activity = Activity::first();
+
+        $this->assertEquals($activity->subject->id, $thread->id);
+    }
+
+    /** @test */
+    public function test_it_records_activity_when_a_reply_is_created()
+    {
+        $this->signIn();
+
+        $reply = create(Reply::class);
+
+        $this->assertDatabaseHas('activities', [
+            'type' => 'created_reply',
+            'user_id' => auth()->id(),
+            'subject_id' => $reply->id,
+            'subject_type' => Reply::class,
+        ]);
+
+        $this->assertEquals(2, Activity::count());
+    }
+}
